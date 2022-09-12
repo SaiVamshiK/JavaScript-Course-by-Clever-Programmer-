bf71e269c5dd302e653e027284689142a5bd1dde
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-09-12 11:33:45 +0530

    taking input in JS

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,8 +1,24 @@
-console.log("Hello world");
-console.log(a)
-var a = "saivamshi";
+/*
+    console.log("Hello world");
+    console.log(a)
+    var a = "saivamshi";
 
-function add(a,b){
-    console.log(a+b);
-}
-add(2,3)
+    function add(a,b){
+        console.log(a+b);
+    }
+    add(2,3)
+
+    // user input
+
+    // Prompt is used for taking user input
+    // Alert is used for displaying alert messages
+
+    fruit = prompt("What is ur fav fruit?");
+    console.log(fruit);
+
+    food = Number(prompt("What is the price of food?"));
+    tipPercent = Number(prompt("What is tip Percent?"))/100;
+    tipAmt = Number(food*tipPercent);
+    console.log("Tip Amount: "+tipAmt)
+    alert("Tip Amount: "+tipAmt)
+*/
