9b955f55226cb25e18d33cc7bde6f9c467e28861
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-09-12 11:45:00 +0530

    basics in JS

--- yourPlayground.js
+++ yourPlayground.js
@@ -22,3 +22,28 @@
     console.log("Tip Amount: "+tipAmt)
     alert("Tip Amount: "+tipAmt)
 */
+
+/* data types (strings, numbers)
+numbers 👉 1, 5, 10, 100, 2.5
+strings 👉 'hello', "what is up"
+arrays 👉 []
+objects 👉 {}
+boolean 👉 true / false (banks)
+*/
+
+/* Math Operators
+Multiply *
+Divide /
+Exponents **
+Modulo/Remainder %  5%2 = 1
+Add +
+Subtract -
+*/
+
+/* Math Methods
+Floor - Rounds down
+Ceil - Rounds up
+Random - gives you number between 0 and 1
+Math.random() : gives you number between 0 and 1
+Math.random()*100 : gives you number between 0 and 100
+*/
